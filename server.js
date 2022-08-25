7d82522b9a7ed4ae0c1ffcbffa4d05054334111c
amanappin <106663187+amanappin@users.noreply.github.com>
2022-08-25 21:34:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:black;">Java Home App - version-10!!</h1> \n');
+  res.send('<h1 style="color:black;">This is a demo node-app EKS-Fargare-cluster setup by code-pipeline ~ Aman </h1> \n');
 });
 
 app.listen(PORT, HOST);
